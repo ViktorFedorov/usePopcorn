9574bbc5bad88ae4c689883a8bef102e04397ce5
ViktorF <index@user.mail>
2023-08-14 07:21:30 +0300

    star-rating bugfix

--- src/components/rating-scale/rating-scale.js
+++ src/components/rating-scale/rating-scale.js
@@ -3,23 +3,28 @@ import styles from './rating-scale.module.css'
 
 const RatingScale = ({ length = 10 }) => {
   const [rating, setRating] = useState(0)
+  const [tempRating, setTempRating] = useState(0)
 
   return (
-    <ul className={styles.scale}>
-      {Array.from({ length: length }).map((star, index) => (
-        <li
-          className={
-            rating < index ? styles.star : `${styles.star} ${styles.active}`
-          }
-          key={index}
-          onClick={() => setRating(index)}
-          onMouseOver={() => setRating(index)}
-        >
-          {' '}
-        </li>
-      ))}
-      <li>{rating + 1}</li>
-    </ul>
+    <div>
+      <div className={styles.rating}>{tempRating || rating || ''}</div>
+      <ul className={styles.scale} onMouseLeave={() => setTempRating(0)}>
+        {Array.from({ length: length }).map((star, index) => (
+          <li
+            className={
+              tempRating <= index
+                ? styles.star
+                : `${styles.star} ${styles.active}`
+            }
+            key={index}
+            onClick={() => setRating(index + 1)}
+            onMouseOver={() => setTempRating(index + 1)}
+          >
+            {' '}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
